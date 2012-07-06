8fb9391ec9460be8aca6f72e5b232657fc847347
jpitou <pitou.ju@gmail.com>
2012-07-06 16:26:50 +0200

    CSS
    Signed-off-by: jpitou <pitou.ju@gmail.com>

---
+++ conf/evolutions/default/1.sql
@@ -0,0 +1,24 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table User (
+  last_name                 varchar(255),
+  first_name                varchar(255),
+  birth_date                timestamp,
+  email                     varchar(255),
+  phone_number              integer)
+;
+
+
+
+
+# --- !Downs
+
+SET REFERENTIAL_INTEGRITY FALSE;
+
+drop table if exists User;
+
+SET REFERENTIAL_INTEGRITY TRUE;
+
